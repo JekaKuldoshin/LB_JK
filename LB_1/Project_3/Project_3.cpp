db22a571c09c63416a88afe34df40c01832d37e4
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-16 14:22:49 +0300

    Программа работатет исправно

---
+++ LB_1/Project_3/Project_3.cpp
@@ -0,0 +1,22 @@
+﻿//расчитать 8 степень числа используя только три оператора умножения.
+
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+
+
+int main()
+{
+
+    setlocale(LC_ALL, "Rus");
+        int a,b,c,d;
+        printf("Введите число которое нужно возвести в а^8: ");
+        scanf_s("%d", &a);
+        b = a * a;
+        c = b * b;
+        d = c * c;
+
+        printf("Результат: %.d\n",d);
+
+
+}
