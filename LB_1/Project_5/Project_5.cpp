489311fb0ff38e9c01b541f97851daefda475c8e
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-16 20:30:51 +0300

    ---

--- LB_1/Project_5/Project_5.cpp
+++ LB_1/Project_5/Project_5.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
+
+
+	printf("Кто загадает большее число, тот и выиграл.\n\n");
+
+
+	int human, pc;
+	printf("Введите переменную а: ");
+	scanf_s("%d",&human);
+	printf("%d",human);
+
+
+
 
 
 }
