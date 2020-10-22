097c8025e231eb4c0f568ff1d9db996dd00a67bc
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-10-22 13:56:30 +0300

    Программа работает исправно

---
+++ LB__1/Project__1/Project__1/Project__1.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<stdio.h>
+//int, short, long, char, float, double
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	printf("Количество байт памяти: int %d \n", sizeof(int));
+	printf("Количество байт памяти: short %hd\n", sizeof(short));
+	printf("Количество байт памяти : long %ld\n", sizeof(long));
+	printf("Количество байт памяти: char %c\n", sizeof(char));
+	printf("Количество байт памяти: float %f\n", sizeof(float));
+	printf("Количество байт памяти: double %lf\n", sizeof(double));
+
+
+}
