c8705d8f6256babcdc5efe289f037c24ca7e76dc
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-11-09 11:08:53 +0200

    Добавил проверку на 0

--- LB__3/LB__3.cpp
+++ LB__3/LB__3.cpp
@@ -1,7 +1,7 @@
 ﻿#define  _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "windows.h"
-using namespace std;
+
 
 int main(void) {
 	SetConsoleCP(1251);
@@ -19,23 +19,31 @@ int main(void) {
 
 
 
-	printf("Введите x >");
+	printf("Введите x > ");
 	scanf("%lf", &x);
 
 
-	
+	if (x == 0)
+	{
+	printf("Вы ввели 0!!!\n");
+	printf("Пожалуйста введите другое число!\n\n");
+	}
+	else
+	{
 
 	bx = b * x;
-	t1 = (1/pow(a,4)) * ((pow(x,3) / 3) -3* bx + 3 * pow(b,2)* log(x + (pow(b,3)) / x)); 
+	t1 = (1 / pow(a, 4)) * ((pow(x, 3) / 3) - 3 * bx + 3 * pow(b, 2) * log(x + (pow(b, 3)) / x));
 
-	
+
 	/*t1 = (pow(x, 3) - 9 * b * x + 9 * pow(9, 2) * log(x + (pow(b, 3) / x))) / (3 * pow(a, 4));*/
+		
 
-
-		ax = a * x;
-		t2 = (1 / (1 - sin(ax))) + (1 / a) * tan(ax / 2);
+	ax = a * x;
+	t2 = (1 / (1 - sin(ax))) + (1 / a) * tan(ax / 2);
 
 	printf("t1 = %lg\n", t1);
 	printf("t2 = %lg\n", t2);
+	
+	}
 	return 0;
 }
