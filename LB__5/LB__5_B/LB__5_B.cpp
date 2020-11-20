0b3416e9aae12b1a18dad5449c7b5bbd2cc5a545
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-11-20 17:10:37 +0200

    +++

--- LB__5/LB__5_B/LB__5_B.cpp
+++ LB__5/LB__5_B/LB__5_B.cpp
@@ -1,35 +1,43 @@
-﻿#define  _CRT_SECURE_NO_WARNINGS
+﻿
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
-#include "windows.h"
 
+int main()
+{
+	system("chcp 1251");
+	system("cls");
 
-
-
-int main(void) {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
-	unsigned k;
-
-	
-		printf("Введите число k > ");
+	unsigned int k;
+	while (true) {
+		system("cls");
+		printf("Введите k > ");
 		scanf("%u", &k);
+
 		
-		if (k % 100 == 10 || k % 100 == 11)
-
+		if ((k % 10 > 4) || (k % 10 == 0) || (k % 100 == 11) || (k % 100 == 12) || (k % 100 == 13) || (k % 100 == 14))
 		{
-			printf("У меня %d грибов", k);
-
-			if (k % 100 == 1)
-			{
-				printf("У меня %d гриб", k);
-
-				if (k % 100 >= 2)
-				{
-					printf("У меня %d грибa", k);
-				}
-			}
+			printf("У меня %u грибов", k);
+			goto link;
 		}
+
+		if (k % 10 == 1)
+		{
+			printf("У меня %u гриб", k);
+			goto link;
+		}
+
+		if ((k % 100 == 2) || (k % 100 == 3) || (k % 100 == 4));
+		{
+			printf("У меня %u гриба", k);
+			goto link;
+		}
+		link:
+
+
+		_getch();
+	}
+
 }
