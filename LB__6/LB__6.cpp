8dbabbb2c7b31aedaf03f4a09aafb6f4e3cac6de
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-11-24 22:15:07 +0200

    +++

--- LB__6/LB__6.cpp
+++ LB__6/LB__6.cpp
@@ -1,43 +1,35 @@
 ﻿#define  _CRT_SECURE_NO_WARNINGS
 
+#include "windows.h"
 #include <stdio.h>
 #include <math.h>
-#include "windows.h"
+#include <conio.h>
 int main(void) {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    long n;
-    double dbln;
-    double term;
-    double sum=0;
-    long k2 = 1;
-    short k1 = 1;
-    const double eps = 0.000001;
 
-    for (n=0; ;n++, k1 = -k1 , k2 = 2 * k2)
-    {
-        dbln = n;
-        term = k1 * 1 - ((k2) / (k2 + 1));
-        if (fabs(term) >= eps) // функция fabs нужно для приведения |term|
-        {
-            sum= sum + term;
-        }
-        else
-        {
-            break;
-        }
 
+    long n;              
+    double sum = 0;        
+    double term;        
+    const double eps = 0.000001; //Точность сравнения в виде числа с плавающей точкой двойной точности (задаем переменной число с не изменным числом)
+    
+      
+    for (n = 0; ; n++) {
+
+        term = pow(-1, n) * (1 - ((pow(2, n)) / (pow(2, n) + 1)));
+ 
+        if ((fabs(term)) >= eps)   // |term|-поставили в модуль для сравнения с ерs
+           
+            sum += term;
+        else break;
+        
         if (n == 9)
-        {
-            printf("Сумма 10 членов ряда = %10.7lf\n", sum);
-            break;
-        }
-
-
-
-
+            printf("Сумма 10 членов ряда = %1.7lf\n", sum);
     }
-    printf("Полная сумма ряда = %lf\n", sum);
+    
+    printf("Полная сумма ряда = %1.7lf\n", sum);
+    _getch();
     return 0;
 }
