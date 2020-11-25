c77d1ca533e9caf5ad7fe6715a5aa4022264da51
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-11-25 13:12:02 +0200

    +++

--- LB__6/LB__6_A/LB__6_A.cpp
+++ LB__6/LB__6_A/LB__6_A.cpp
@@ -15,29 +15,66 @@
 #include <math.h>
 #include <conio.h>
 #include <time.h>
-int main(void) {
+int main(void) 
+{
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int i;
     int my_num;
-    int comp_num;
+    srand(time(NULL));
+    int comp_num = 1 + rand() % 100;
+    int count = 0;
 
-    for(my_num=comp_num;)
-        srand(time(NULL));
-        int comp_num = 1 + rand() % 100;
-
-        printf("%d", comp_num);
-
+    printf("|\t                           PC    VS    HUMAN                        |\n");
+    printf("|===========================================================================|\n");
+    printf("|                                                                           |\n");
+    printf("|\t\t\tПопробуй угадать число ПК до 10                     |\n");
+    printf("|                                                                           |\n");
+    printf("|===========================================================================|\n");
 
     
 
+    do
+    {
+        link:
+        int comp_num = 1 + rand() % 10;
+
+        printf("Введите число >");
+        scanf("%d", &my_num);
+        count++;
+
+        if (my_num == comp_num)
+        {
+            printf("Поздравляем! Вы угадали с %d попытки, число загаданное PC = %d\n\n", count, comp_num);
+            break;
+        }
+        if (my_num > 10)
+        {
+            printf("ERROR!! PC создает число до 10!\n\n");
+            goto link;
+        }
+
+        if (my_num <= comp_num)
+        {
+            printf("Вы ввели число меньше, попробуйте в следующий раз больше. PC=%d\n\n", comp_num);
+        }
+
+        if (my_num >= comp_num)
+        {
+            printf("Вы ввели число большe, попробуйте в следующий раз меньше. PC=%d\n\n", comp_num);
+        }
+        
 
 
-
-
-
-
+    }
+    while (my_num != comp_num); //no
+    _getch();
+    return 0;
 }
+
+
+
+
+
 
 
