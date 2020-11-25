5b15b0a6074d5b2219a70e265a2e3653be46a3ef
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-11-25 19:44:31 +0200

    +++

---
+++ LB__6/LB__6_B/LB__6_B.cpp
@@ -0,0 +1,33 @@
+﻿//ЛАБОРАТОРНАЯ РАБОТА № 6Б
+//
+//Проверка знаний таблицы умножения(10 примеров, случайным образом; выставляется оценка).
+
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include "windows.h"
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <time.h>
+int main(void)
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int my_num;
+    srand(time(NULL));
+    int comp_num = 1 + rand() % 100;
+    int count = 0;
+
+    printf("|===========================================================================|\n");
+    printf("|                                                                           |\n");
+    printf("|                   Проверь свои знания таблицы умножения                   |\n");
+    printf("|                                                                           |\n");
+    printf("|===========================================================================|\n");
+
+    do
+
+
+
+
+}
