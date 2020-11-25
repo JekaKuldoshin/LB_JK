8da3d85f6b9155a3f47059f41088538ce6ef1230
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-11-25 21:46:10 +0200

    +++

--- LB__6/LB__6_B/LB__6_B.cpp
+++ LB__6/LB__6_B/LB__6_B.cpp
@@ -14,10 +14,7 @@ int main(void)
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int my_num;
-    srand(time(NULL));
-    int comp_num = 1 + rand() % 100;
-    int count = 0;
+    
 
     printf("|===========================================================================|\n");
     printf("|                                                                           |\n");
@@ -25,9 +22,58 @@ int main(void)
     printf("|                                                                           |\n");
     printf("|===========================================================================|\n");
 
-    do
+    int pc_result, human_result, N1, N2;
+    int count = 0;
+
+
+    for (int i = 0; i < 10; i++)
+    {
+        srand(time(NULL));
+      N1= rand() % 10;
+      N2= rand() % 10;
+      pc_result = N1 * N2;
+
+      printf("\n%d * %d =", N1, N2);
+      scanf("%d", &human_result);
+
+      if (human_result != pc_result)
+      {
+          printf("Вы допустили ошибку!\n");
+          continue;
+      }
+      else
+      {
+          printf("Правильно, вам начисленно +1 бал\n\n");
+          count++;
+      }
 
 
 
 
+    }
+
+    if ((count % 10 > 4) || (count % 10 == 0))
+    {
+        printf("Поздравляем! Вы заработали %d баллов", count);
+        goto link;
+    }
+
+    if (count % 10 == 1)
+    {
+        printf("\nВы заработали %d балл!\nКак выучишь таблицу умножения приходи :)\n", count);
+        goto link;
+    }
+
+    if ((count % 100 == 2) || (count % 100 == 3) || (count % 100 == 4))
+    {
+        printf("\nВы заработали %d балла!\nКак выучишь таблицу умножения приходи :)\n", count);
+        goto link;
+    }
+link:
+
+
+    _getch();
+    return 0;
 }
+
+
