fbd92266be3c53e9c06226af9dec0e0b14ca628d
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-12-10 09:33:40 +0200

    +++

--- LB__8/LB__8/LB__8.cpp
+++ LB__8/LB__8/LB__8.cpp
@@ -13,20 +13,20 @@ int main()
 
 	int const SIZE = 100;
 	int arr[SIZE];
-	int avar = 0;
+	int avar = 0; //среднее арифметическое
 	int sum = 0;
-	int q = 0;
+	int q = 0; //количество элементов в массиве
 
 	cout << "Начальный массив:" << endl;
 	for (int i = 0; i < SIZE; i++)
 	{
-		arr[i] = rand() % (100 - (-100) + 1) + (-100);
+		arr[i] = rand() % (100 - (-100) + 1) + (-100); //рандомные числа от -100 до 100
 
 	}
 
 	for (int i = 0; i < SIZE; i++)
 	{
-		cout << setw(4) << arr[i];
+		cout << setw(4) << arr[i];   //выводим начальный массив 
 		cout << " ";
 	}
 	cout << endl << endl;
@@ -39,18 +39,18 @@ int main()
 	{
 		if (arr[i] >= 0)
 		{
-			sum += arr[i];
-			q += 1;
+			sum += arr[i];   //добавляем каждый элемент массива
+			q += 1;         //считаем количество элементов
 		}
 
 	}
 
 
-	avar = sum / q;
+	avar = sum / q;          //находим среденее арифметическое 
 
 	for (int i = 0; i < SIZE; i++)
 	{
-		if (arr[i] <= 0)
+		if (arr[i] <= 0)   //все что меньше нуля, происходит замена на среднее ариметическое 
 		{
 			arr[i] = avar;
 		}
