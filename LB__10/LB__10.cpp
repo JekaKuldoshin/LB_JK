79ab0f77758eaff6aa160f8b01a3fa133d180a67
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2020-12-14 17:35:07 +0200

    +++

---
+++ LB__10/LB__10.cpp
@@ -0,0 +1,74 @@
+﻿//8. Поменять числа главной и боковой диагоналей местами[123567890] = > [321567098]
+
+#include <iostream>
+#include<math.h>
+#include<conio.h>
+#include<time.h>
+#include<iomanip>
+
+
+#define N 3
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int b;
+
+	cout << "Поменяйте числа главной и боковой диагоналей местами" << endl<<endl;
+	
+
+	cout << "Начальный массив: " << endl;
+
+	int arr[N][N]
+	{
+		{1,2,3},
+		{5,6,7},
+		{8,9,0},
+
+	};
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << setw(4) << arr[i][j];
+		}
+		cout << endl;
+	}
+	cout << "\nМодернизированный массив: " << endl;
+	for (int i = 0; i < N; i++)
+
+	{
+
+		for (int j = 0; j < N; j++)
+			if (i == j )
+			{
+
+				b = arr[i][j];
+
+				arr[i][j] = arr[i][N - 1 - j];
+
+				arr[i][N - 1 - j] = b;
+
+			}
+
+
+	}
+
+	
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << setw(4) << arr[i][j];
+		}
+		cout << endl;
+	}
+	_getch();
+	return 0;
+
+
+}
---
+++ LB__9/LB__9.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <time.h>
+#include<conio.h>
+#include <iomanip>
+
+using namespace std;
+
+#define N 10
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+
+
+    int a[N][N], i, j, b;
+    
+
+    cout << "Начальный массив: " << endl;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            a[i][j] = rand() % 10 - 5;
+            cout << setw(4) << a[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < N; i++)
+
+    {
+
+        for (int j = 0; j < N; j++)
+            if(i == j )
+        {
+
+            b = a[i][j];
+
+            a[i][j] = a[i][N - 1 - j];
+
+            a[i][N - 1 - j] = b;
+
+        }
+            
+
+    }
+
+    cout << "Модернизированный массив: " << endl;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            cout << setw(4) << a[i][j];
+        }
+        cout << endl;
+    }
+    _getch();
+    return 0;
+}
