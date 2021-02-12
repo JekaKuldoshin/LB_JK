2e428b1e31e36c99d774118acb0ee98edf4b6361
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-02-12 13:56:12 +0200

    +++

---
+++ LB_2__1/LB__1/LB__1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	cout << "        Меню" << endl;
+	cout << "------------------" << endl;
+	switch
+
+		cout << "1 – ввод с экрана\n2 – случайным образом" << endl;
+
+
+	cout << "Сделайте выбор -> " << endl;
+
+}
