89e9757db46e423bb510192145afec4db8f9d59c
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-02-23 16:30:09 +0200

    +++

---
+++ LB__2__2/Main/Sort.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "struct.h"
+
+void Sort(mon spis[]);
---
+++ LB__2__2/Main/Struct.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include <time.h>
+#include <string>
+#include<conio.h>
+using namespace std;
+
+#define N 3
+
+struct mon
+{
+
+	int sc;                       /*год*/
+	string name;                  /*имя*/
+	unsigned int size;            /*диаметр*/
+	unsigned int mhz;             /*частота*/
+};
