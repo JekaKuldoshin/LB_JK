93b35c49b52da4811d695ed80a1d1e478c3c34dd
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-02-25 13:50:45 +0200

    +++

--- LB__2__2/Main/Main.cpp
+++ LB__2__2/Main/Main.cpp
@@ -21,22 +21,8 @@ link_menu1:
 
 	cout << "Сделайте выбор -> ";
 	cin >> selection;
-
-	if (selection == 1)
-	{
-		Bring(spis, N, selection);     /*Ввод с консоли*/
-		goto link_menu2;
-	}
-	if (selection == 2)
-	{
-		byChance(spis, N, selection);  /* Ввод рандомно*/
-	}
-	else
-	{
-		cout << "\nОшибка! Пункт меню отсутствует!" << endl;
-		_getch();
-		goto link_menu1;
-	}
+	Choice_1
+	
 
 	/*------------------------------------------------------------*/
 link_menu2:
@@ -50,33 +36,11 @@ link_menu2:
 
 	cout << "Сделайте выбор -> ";
 	cin >> selection;
+	Choice_2
 
-	if (selection == 1)
-	{
-		Sort(spis);            /*Сделали обращение к функции сортировки*/
-		Print(spis);          /* Обратились к функции вывода*/
-		goto link_menu2;
-	}
-	if (selection == 2)
-	{
 
-		Print(spis);        /* Обратились к функции вывода*/
-		goto link_menu2;
-	}
-	if (selection == 3)
-	{
-		goto link_menu1;       /* Переходы по программе*/
-	}
-	if (selection == 4)
-	{
-		goto link_exit;       /* Переходы по программе*/
-	}
-	else
-	{
-		cout << "\nОшибка! Пункт меню отсутствует!" << endl;
-		_getch();
-		goto link_menu2;
-	}
+
+	
 link_exit:
 	system("cls");
 	cout << "Thanks for your attention!!" << endl;
--- LB__2__2/Main/Struct.h
+++ LB__2__2/Main/Struct.h
@@ -7,6 +7,8 @@
 #include<conio.h>
 using namespace std;
 
+#define PRINT_TYPE
+
 #define N 3
 
 struct mon
@@ -17,3 +19,22 @@ struct mon
 	unsigned int size;            /*диаметр*/
 	unsigned int mhz;             /*частота*/
 };
+
+#ifndef Choice_1 
+#define Choice_1 if (selection == 1){Bring(spis, N, selection);     /*Ввод с консоли*/	goto link_menu2;}if (selection == 2){	byChance(spis, N, selection);  /* Ввод рандомно*/}else{	cout << "\nОшибка! Пункт меню отсутствует!" << endl;_getch();	goto link_menu1;}
+#endif
+
+#ifndef Choice_2
+#define Choice_2 if (selection == 1) { Sort(spis);            /*Сделали обращение к функции сортировки*/ Print(spis);          /* Обратились к функции вывода*/ goto link_menu2; } if (selection == 2) {	Print(spis);        /* Обратились к функции вывода*/	goto link_menu2;}if (selection == 3){	goto link_menu1;       /* Переходы по программе*/}if (selection == 4){	goto link_exit;       /* Переходы по программе*/}else{	cout << "\nОшибка! Пункт меню отсутствует!" << endl;	_getch();	goto link_menu2;}
+#endif
+
+#ifndef DEBUG_1
+#define DEBUG_1 cout << "| " __DATE__ <<" |"<< endl <<"| " <<  __TIME__ << "    |" << endl <<"---------------" << endl;
+#endif
+
+#ifndef DEBUG_2
+#define DEBUG_2 cout << endl << "Файл: " << __FILE__ << endl << "Функция: " << __FUNCTION__ << endl << endl;
+#endif
+
+
+//cout << "| " __DATE__ <<" |"<< endl <<  __TIME__ << " |" << endl <<"----------------------------" << endl;
