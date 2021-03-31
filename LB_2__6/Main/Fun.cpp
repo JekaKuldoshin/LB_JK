cf4bb785c21f0837da5baf182bf2281e9d1d78cf
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-03-31 14:52:46 +0300

    +++

---
+++ LB_2__6/Main/Fun.cpp
@@ -0,0 +1,20 @@
+#include "Fun.h"
+
+void delete_str(char* str, int where, int what) {
+
+    int i;
+    for (i = 0; str[i]; i++);
+    {
+        int j = where - 1;
+
+        while (str[j]) {
+
+
+            str[j] = str[j + what];
+            j++;
+
+        }
+        printf(str);
+    }
+
+}
