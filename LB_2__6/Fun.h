bde79c50662ba5f027cc3cb1e8b6d82c0b2dc9c5
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2021-03-31 14:44:47 +0300

    +++

---
+++ LB_2__6/Fun.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef FX
+#define FX
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <Windows.h>
+
+#define N 100
+
+void delete_str(char* str, int where, int what);
+#endif
